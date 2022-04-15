52cd27dc13a2fdb6b298bf33b1332799a333458f
Germain Mathis <mgermain@ca-cf.fr>
2022-04-15 10:03:20 +0200

    ajout list role

---
+++ SessionDasso/src/main/java/com/esgi/alasso/model/role/User_role.java
@@ -0,0 +1,23 @@
+package com.esgi.alasso.model.role;
+
+public class User_role {
+
+    private String role_id;
+    private String user_id;
+
+    public String getRole_id() {
+        return role_id;
+    }
+
+    public void setRole_id(String role_id) {
+        this.role_id = role_id;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+}
--- SessionDasso/src/main/java/com/esgi/alasso/model/utilisateur/Utilisateur.java
+++ SessionDasso/src/main/java/com/esgi/alasso/model/utilisateur/Utilisateur.java
@@ -1,9 +1,14 @@
 package com.esgi.alasso.model.utilisateur;
 
+import com.esgi.alasso.model.role.User_role;
+
+import java.util.List;
+
 public class Utilisateur {
 
     private String id;
     private String email;
+    private List<User_role> roles;
 
     public String getId() {
         return id;
@@ -19,5 +24,13 @@ public class Utilisateur {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<User_role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<User_role> roles) {
+        this.roles = roles;
     }
 }
